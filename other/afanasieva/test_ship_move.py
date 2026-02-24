5cc9403fb64e4616d3244891fdbfab046a733dd5
AfanAlex01 <alex.afanas01@gmail.com>
2026-02-24 11:12:26 +0300

    Changes to be committed: modified:   other/afanasieva/test_ship_move.py

--- other/afanasieva/test_ship_move.py
+++ other/afanasieva/test_ship_move.py
@@ -5,7 +5,6 @@ app = Ursina()
 camera.position = (0, 60, 0)
 camera.rotation = (90, 0, 0)
 
-
 ground = Entity(
     model='plane', 
     scale=(20, 1, 20), 
@@ -21,6 +20,16 @@ cube = Entity(
     collider='box'
 )
 
+snap_points = [
+    Vec3(5, 0.5, 5),
+    Vec3(-5, 0.5, 5),
+    Vec3(5, 0.5, -5),
+    Vec3(-5, 0.5, -5)
+]
+
+for p in snap_points:
+    Entity(model='sphere', color=color.white, scale=0.5, position=p)
+
 is_dragging = False
 
 def input(key):
@@ -31,13 +40,24 @@ def input(key):
             is_dragging = True
 
     if key == 'left mouse up':
-        is_dragging = False
+        if is_dragging:
+            is_dragging = False
+            nearest_point = snap_points[0]
+            min_dist = distance(cube.position, snap_points[0])
+
+            for p in snap_points:
+                dist = distance(cube.position, p)
+                if dist < min_dist:
+                    min_dist = dist
+                    nearest_point = p
+            
+            cube.position = nearest_point
 
 def update():
     global is_dragging
 
     if is_dragging and held_keys['left mouse']:
         if mouse.world_point:
-            cube.position = (mouse.world_point.x, cube.y, mouse.world_point.z)
+            cube.position = Vec3(mouse.world_point.x, cube.y, mouse.world_point.z)
 
 app.run()
